90b80bd3b3537e522132331139863f3d0fae4407
Frédérik Paradis <fredy_14@live.fr>
2015-08-29 10:44:46 -0400

    modifying logout management

--- app/src/main/java/org/findadoge/app/util/Util.java
+++ app/src/main/java/org/findadoge/app/util/Util.java
@@ -1,19 +1,32 @@
 package org.findadoge.app.util;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 
 import org.findadoge.app.DispatchActivity;
+import org.findadoge.app.R;
 
 
 public class Util {
-    public static void logout(Context packageContext) {
-        ParseUser.logOut();
+    public static void logout(final Context packageContext) {
+        final ProgressDialog dialog = new ProgressDialog(packageContext);
+        dialog.setMessage(packageContext.getString(R.string.progress_logout));
+        dialog.show();
 
-        Intent intent = new Intent(packageContext, DispatchActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        packageContext.startActivity(intent);
+        ParseUser.logOutInBackground(new LogOutCallback() {
+            @Override
+            public void done(ParseException e) {
+                dialog.dismiss();
+
+                Intent intent = new Intent(packageContext, DispatchActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                packageContext.startActivity(intent);
+            }
+        });
     }
 }
